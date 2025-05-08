85fa20e1254eb3160bce35bf71b9df2d1686e39d
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-05-08 08:36:33 -0300

    Atualizei o repositório

--- mundo001/ex003.py
+++ mundo001/ex003.py
@@ -1,4 +1,9 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-soma = n1 + n2
-print(f'A soma entre {n1} e {n2} é {soma}.')
+def soma(n1, n2):
+    s = n1 + n2
+    return f'A soma entre {n1} e {n2} é {s}'
+
+
+x1 = int(input('Digite um valor: '))
+x2 = int(input('Digite outro valor: '))
+tot = soma(x1, x2)
+print(tot)
