49b0fae8c1e010d623d9401172dcea731d4eff46
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-07-01 14:56:43 -0300

    atualizei o repositório

--- desafios/de006.py
+++
@@ -1,8 +0,0 @@
-#Crie um programa que leia um valor e mostre seu dobro, triplo e raiz quadrada
-
-
-n = int(input('Digite um valor: '))
-dobro = n*2
-triplo = n*3
-raiz = n**(1/2)
-print(f'O valor digitado foi {n}\nSeu dobro vale {dobro}\nO triplo vale {triplo}\nSua raiz quadrada é {int(raiz)}')
--- desafios/de012.py
+++
@@ -1,6 +0,0 @@
-#Crie um algoritmo que leia o preço de um produto e mostre seu preço com 5% de desconto
-
-
-preco = float(input('Digite o valor do produto: R$'))
-novo_preco = preco - (preco * 0.05)
-print(f'O valor do produto é R${preco:.2f}.\nCom desconto o valor ficará R${novo_preco:.2f}')
