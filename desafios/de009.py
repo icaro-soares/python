f32505b3572c230cac69a7a984fbef8bb5a4f85c
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-07-01 12:46:18 -0300

    atualizei o repositório

--- desafios/de009.py
+++ desafios/de009.py
@@ -1,8 +1,17 @@
-#Crie um programa que leia um número inteiro e mostre sua tabuada
-cont = 0
+#Crie um programa que leia um número inteiro e mostre sua tabuada sem usar estruturas de repetição
+
+
 n = int(input('Digite um número inteiro: '))
 print(f'Tabuada de {n}')
 print('-'*20)
-while (cont <= 10):
-    print(f'{n} x {cont} = {n*cont}')
-    cont+=1
+print(f'{n} x 0 = {n*0:>2}')
+print(f'{n} x 1 = {n*1:>2}')
+print(f'{n} x 2 = {n*2:>2}')
+print(f'{n} x 3 = {n*3:>2}')
+print(f'{n} x 4 = {n*4:>2}')
+print(f'{n} x 5 = {n*5:>2}')
+print(f'{n} x 6 = {n*6:>2}')
+print(f'{n} x 7 = {n*7:>2}')
+print(f'{n} x 8 = {n*8:>2}')
+print(f'{n} x 9 = {n*9:>2}')
+print(f'{n} x 10 = {n*10:>2}')
