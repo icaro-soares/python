351f349b2d31a1b88c739778be3104e883e7f40b
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-07-02 11:24:24 -0300

    atualizei o repositório

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,7 +1,6 @@
 #Faça um programa em python que leia e reproduza um arquivo mp3
 import pygame
 
-
-musica = pygame.mixer.music.load('ex021.mp3')
+musica = 'ex021.mp3'
 pygame.mixer.music.play(musica)
 pygame.mixer.music.stop()
