ce184763aa3c7367867397e80e510ed3af73bb2e
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-09-21 10:19:01 -0300

    Atualizei o repositório

--- ex020.py
+++ ex020.py
@@ -1,11 +1,13 @@
-from random import choices
-
-
 #O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+
+from random import shuffle
+
+
 aluno_um = str(input('Digite o nome do 1º aluno(a): '))
 aluno_dois = str(input('Digite o nome do 2º aluno(a): '))
 aluno_três = str(input('Digite o nome do 3º aluno(a): '))
 aluno_quatro = str(input('Digite o nome do 4º aluno(a): '))
-lista_alunos = [aluno_um, aluno_dois, aluno_três, aluno_quatro]
-ordem_alunos = choices(lista_alunos)
-print(ordem_alunos)
+alunos = [aluno_um, aluno_dois, aluno_três, aluno_quatro]
+ordem_apresentação = shuffle(alunos)
+print(f'A ordem de apresentção de trabalhos será: {alunos}')
