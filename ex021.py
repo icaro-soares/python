42dfa2c49848798b87eb3dda8e6e48e2daa8c1ab
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-09-21 10:30:57 -0300

    Atualizei o repositório

---
+++ ex021.py
@@ -0,0 +1,12 @@
+#Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+
+import pygame
+
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+input()
+pygame.quit()
