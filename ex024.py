aa79fbf4bd3559611757e83571e917f84f223c2a
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-09-25 13:43:18 -0300

    Atualizei o repositório

--- ex024.py
+++ ex024.py
@@ -1,1 +1,6 @@
 #Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO"
+
+cidade = str(input('Digite o nome de uma cidade: ')).title().strip()
+print(f'O nome da cidade digitada foi {cidade}')
+cidade_separado = cidade.split()
+print('Santo' in cidade_separado[0])
