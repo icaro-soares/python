bdb6bba6a85f6630bf1c2fbef0e9534c3075ff73
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-09-25 10:15:29 -0300

    Atualizei o repositório

--- ex022.py
+++ ex022.py
@@ -2,13 +2,3 @@
 #- O nome com todas as letras maiúsculas e minúsculas.
 #- Quantas letras ao todo (sem considerar espaços).
 #- Quantas letras tem o primeiro nome.
-
-
-nome = str(input('Qual seu nome completo? ')).strip()
-nome_separado = nome.split()
-tam_nome = len(nome)
-tam_primeiro_nome = len(nome_separado[0])
-
-print(f'Seu nome em letras maiúsculas é {nome.upper()}\nE em letras minúsculas é {nome.lower()}')
-print(f'Seu nome tem {tam_nome} letras ao todo')
-print(f'O primeiro nome tem {tam_primeiro_nome}')
