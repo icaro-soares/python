53030d8099c75b6f20b1c4a667dc95ec2e632872
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-09-26 08:18:59 -0300

    Atualizei o repositório

--- ex025.py
+++ ex025.py
@@ -1,6 +1,1 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
-
-nome_completo = str(input('Digite seu nome completo: ')).title().strip()
-print(f'Muito prazer {nome_completo}')
-nome_silva = 'Silva' in nome_completo
-print(nome_silva)
