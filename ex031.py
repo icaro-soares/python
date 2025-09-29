c506a6c91500d702ccfa0d9eb509a6c8d7c36061
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-09-29 09:56:23 -0300

    Atualizei o repositório

--- ex031.py
+++ ex031.py
@@ -1,1 +1,8 @@
 #Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
+distância = float(input('Qual a distância para seu destino (km): '))
+if distância <= 200:
+    preço = distância * 0.50
+    print(f'O preço da viagem será de R${preço:.2f}')
+else:
+    preço = distância * 0.45
+    print(f'O preço da viagem será de R${preço:.2f}')
