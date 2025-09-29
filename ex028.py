473913b6db7c9e8bc7d42b38ce003399df801f68
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-09-29 09:44:01 -0300

    Atualizei o repositório

--- ex028.py
+++ ex028.py
@@ -1,1 +1,11 @@
 #Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. 
+from random import randint
+
+
+aleatório = randint(0, 5)
+print('Pensei em um número entre 0 e 5, tente adivinhá-lo!')
+chute = int(input('Seu palpite: '))
+if chute == aleatório:
+    print('Parabéns! Você acertou o número que pensei!')
+else:
+    print(f'Que pena! O número que pensei foi {aleatório}')
