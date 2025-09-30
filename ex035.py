b9b202dbdf779db9c3f1f0b94c65decfbbb34419
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-09-30 09:34:39 -0300

    Atualizei o repositório

--- ex035.py
+++ ex035.py
@@ -1,8 +1,9 @@
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
-lado_a = int(input('Qual a medida do 1º lado (cm): '))
-lado_b = int(input('Qual a medida do 2º lado (cm): '))
-lado_c = int(input('Qual a medida do 3º lado (cm): '))
-if lado_a + lado_b > lado_c or lado_b + lado_c > lado_a:
+r1 = int(input('Qual a medida do 1º lado (cm): '))
+r2 = int(input('Qual a medida do 2º lado (cm): '))
+r3 = int(input('Qual a medida do 3º lado (cm): '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Pode formar um triângulo')
 else:
     print('Não pode formar um triângulo')
+    
