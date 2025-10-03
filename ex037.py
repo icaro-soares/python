b852ad4b9afbd552af6bf7f056e238a3cb0a64dc
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-10-03 09:25:38 -0300

    Atualizei o repositório

--- ex037.py
+++ ex037.py
@@ -1,1 +1,11 @@
 #Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal
+n = int(input('Digite um inteiro qualquer: '))
+escolha = int(input('Escolha o tipo de conversão [1 - binário] [2 - octal] [3 - hexadecimal]: '))
+if escolha == 1:
+    print(f'Número em BINÁRIO: {bin(n)[2:]}')
+elif escolha == 2:
+    print(f'Número em OCTAL: {oct(n)[2:]}')
+elif escolha == 3:
+    print(f'Número em HEXADECIMAL: {hex(n)[2:]}')
+else:
+    print('Escolha inválida, por favor, tente novamente')
