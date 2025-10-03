5f9c0ecbab7a2b4160ffe991ca94c7a6bc5c1772
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2025-10-03 09:52:48 -0300

    Atualizei o repositório

--- ex040.py
+++ ex040.py
@@ -4,3 +4,13 @@ Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando
 - Média entre 5.0 e 6.9: RECUPERAÇÃO
 - Média 7.0 ou superior: APROVADO
 '''
+n1 = float(input('Digite a 1ª nota do(a) aluno(a): '))
+n2 = float(input('Digite a 2ª nota do(a) aluno(a): '))
+m = (n1+n2)/2
+print(f'Sua média final foi {m:.1f}')
+if m < 5:
+    print('ALUNO(A) REPROVADO(A)')
+elif 5 <= m <= 6.9:
+    print('ALUNO(A) DE RECUPERAÇÃO')
+else:
+    print('ALUNO(A) APROVADO(A)')
