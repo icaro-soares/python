b4b3bc25075b7afea87437c2d3939c77c45d262a
Ícaro Soares <icarosa21@gmail.com>
2025-10-07 10:01:12 -0300

    Atualizei o repositório

--- ex045.py
+++ ex045.py
@@ -2,14 +2,23 @@
 from random import randint
 import emoji
 from time import sleep
+
 print('[0] Pedra')
 print('[1] Papel')
 print('[2] Tesoura')
-itens = ('Pedra', 'Papel', 'Tesoura')
+itens = (emoji.emojize(':punho_levantado:', language='pt'), emoji.emojize(':dorso_da_mão_levantado:', language='pt'), emoji.emojize(':mão_em_v_de_vitória:', language='pt'))
 computador = randint(0, 2)
 jogador = int(input('Faça sua jogada: '))
+print('JO')
+sleep(.5)
+print('KEN')
+sleep(.5)
+print('PÔ!!!')
+print('='*11)
 print(f'Jogador escolheu {itens[jogador]}')
 print(f'Computador escolheu {itens[computador]}')
+print('='*11)
+
 if computador == 0: #computador joga pedra
     if jogador == 0: #jogador joga pedra
         print('Empatou!')
@@ -19,11 +28,11 @@ if computador == 0: #computador joga pedra
         print('Computador vence!')
 elif computador == 1: #computador joga papel
     if jogador == 0:
-        print('Jogador vence!')
+        print('Computador vence!')
     elif jogador == 1:
         print('Empatou!')
     elif jogador == 2:
-        print('Computador vence!')
+        print('Jogador vence!')
 elif computador == 2: #computador joga tesoura
     if jogador == 0:
         print('Jogador vence!')
