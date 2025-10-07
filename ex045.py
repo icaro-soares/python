d1572b7e2b01c7746a8ff51c72c5ddbde69708e7
Ícaro Soares <icarosa21@gmail.com>
2025-10-07 09:37:55 -0300

    Atualizei o repositório

--- ex045.py
+++ ex045.py
@@ -2,53 +2,32 @@
 from random import randint
 import emoji
 from time import sleep
-
-j1 = int(input('Escolha sua jogada\n[ 0 ] Pedra\n[ 1 ] Papel\n[ 2 ] Tesoura: '))
-if j1 == 0:
-    escolha_j1 = emoji.emojize(':punho_levantado:', language='pt')
-elif j1 == 1:
-    escolha_j1 = emoji.emojize(':mão_aberta_com_os_dedos_separados:', language='pt')
-elif j1 == 2:
-    escolha_j1 = emoji.emojize(':mão_em_v_de_vitória:', language='pt')
-else:
-    print('Escolha inválida')
-    exit()
-
-cpu = randint(0, 2)
-if cpu == 0:
-    escolha_cpu = emoji.emojize(':punho_levantado:', language='pt')
-elif cpu == 1:
-    escolha_cpu = emoji.emojize(':mão_aberta_com_os_dedos_separados:', language='pt')
-elif cpu == 2:
-    escolha_cpu = emoji.emojize(':mão_em_v_de_vitória:', language='pt')
-
-print('JO')
-sleep(.5)
-print('KEN')
-sleep(.5)
-print('PO')
-sleep(.5)
-print('='*20)
-print(f'O jogador escolheu {escolha_j1}')
-print(f'A cpu escolheu {escolha_cpu}')
-print('='*20)
-
-#empate
-if j1 == cpu:
-    print('Empatou'.upper())
-
-#j1 vence
-if j1 == 0 and cpu == 2:
-    print('O jogador 1 venceu!')
-elif j1 == 1 and cpu == 0:
-    print('O jogador 1 venceu!')
-elif j1 == 2 and cpu == 1:
-    print('O jogador 1 venceu!')
-
-#cpu vence
-if cpu == 0 and j1 == 2:
-    print('A cpu venceu!')
-elif cpu == 1 and j1 == 0:
-    print('A cpu venceu!')
-elif cpu == 2 and j1 == 1:
-    print('A cpu venceu!')
+print('[0] Pedra')
+print('[1] Papel')
+print('[2] Tesoura')
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+jogador = int(input('Faça sua jogada: '))
+print(f'Jogador escolheu {itens[jogador]}')
+print(f'Computador escolheu {itens[computador]}')
+if computador == 0: #computador joga pedra
+    if jogador == 0: #jogador joga pedra
+        print('Empatou!')
+    elif jogador == 1: #jogador joga papel
+        print('Jogador vence!')
+    elif jogador == 2: #jogador joga tesoura
+        print('Computador vence!')
+elif computador == 1: #computador joga papel
+    if jogador == 0:
+        print('Jogador vence!')
+    elif jogador == 1:
+        print('Empatou!')
+    elif jogador == 2:
+        print('Computador vence!')
+elif computador == 2: #computador joga tesoura
+    if jogador == 0:
+        print('Jogador vence!')
+    elif jogador == 1:
+        print('Computador vence!')
+    elif jogador == 2:
+        print('Empatou!')
