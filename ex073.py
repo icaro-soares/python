6e498eff9458d613c1f63a8738de4e02c0767214
Ícaro Soares <icarosa21@gmail.com>
2025-11-02 10:34:45 -0300

    Atualizei o repositório

--- ex073.py
+++ ex073.py
@@ -1,1 +1,14 @@
-times = ('Palmeiras', 'Flamengo', 'Cruzeiro', 'Mirassol', 'Bahia', 'Fluminense', 'Botafogo', 'Vasco da Gama', 'São Paulo', 'Corinthians', 'Grêmio', 'Bragantino', 'Atlético-MG', 'Ceará SC', 'Internacional', 'Santos', 'EC Vitória', 'Fortaleza', 'Juventude', 'Sport Recife')
+c = 0
+times = ('Palmeiras', 'Flamengo', 'Cruzeiro', 'Mirassol', 'Bahia', 'Fluminense',
+ 'Botafogo', 'Vasco da Gama', 'São Paulo', 'Corinthians', 'Grêmio', 'Bragantino'
+ , 'Atlético-MG', 'Ceará SC', 'Internacional', 'Santos', 'EC Vitória', 
+ 'Fortaleza', 'Juventude', 'Sport Recife')
+
+for time in range(0, 5):
+    print(f'{c+1}º lugar: {times[time]}')
+    c+=1
+print('='*20)
+
+print('='*20)
+print(sorted(times))
+print('='*20)
