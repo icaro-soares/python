fdb1593a19407a4a41a2516c1d9f619d0b11944c
Ícaro Soares <icarosa21@gmail.com>
2025-11-06 10:57:24 -0300

    Atualizei o repositorio

--- jogos/jogo_adivinhação.py
+++ jogos/jogo_adivinhação.py
@@ -1,4 +1,7 @@
 from random import randint
+
+
+pontos = 1000
 chances = 0
 número_secreto = randint(0, 100)
 dificuldade = int(input('Escolha a dificuldade [1 - Fácil][2 - Médio][3 - Difícil]: '))
@@ -16,12 +19,19 @@ while chances > 0:
     chute = int(input('Tente adivinhar o número secreto: '))
     if chute == número_secreto:
         print('Parabéns! Você acertou!')
+        print(f'Pontuação final: {pontos}')
         break
     elif chute != número_secreto:
         chances-=1
         if chute > número_secreto:
             print('Errou! Tente um número menor')
+            pontos-=chute
+            print(f'Pontuação atual: {pontos}')
         elif chute < número_secreto:
             print('Errou! Tente um número maior')
+            pontos-=chute
+            print(f'Pontuação atual: {pontos}')
         if chances == 0:
             print(f'Suas chances acabaram! O número secreto era {número_secreto}')
+            pontos = 0
+print(f'Pontuação final {pontos}')
