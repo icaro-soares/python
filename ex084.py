8286e9e691b2d853a70e2f5462d17b54c5574e50
Ícaro Soares <icarosa21@gmail.com>
2025-11-09 09:13:34 -0300

    Atualizei o repositório

--- ex084.py
+++ ex084.py
@@ -1,9 +1,17 @@
+geral = []
 pessoas = []
+p_quilo = []
 while True:
     nome = str(input('Qual o nome: ')).title().strip()
     pessoas.append(nome)
+    peso = float(input('Qual o peso [kg]: '))
+    p_quilo.append(peso)
     continuar = ' '
     while continuar not in 'SsNn':
         continuar = str(input('Quer continuar [S/N]? ')).strip()[0]
     if continuar in 'Nn':
         break
+geral.append(pessoas)
+geral.append(p_quilo)
+print(f'Pessoas cadastradas: {len(pessoas)}')
+print(p_quilo)
