7645be34f9b60d273ceb280819214e89d560c6b9
Ícaro Soares <icarosa21@gmail.com>
2025-11-25 08:34:23 -0300

    Atualizei o ex091.py

--- ex091.py
+++ ex091.py
@@ -1,15 +1,22 @@
 from random import randint
+from operator import itemgetter
+from time import sleep
 
 
-num = {}
+num = {
+    'jogador1':randint(1, 6),
+    'jogador2':randint(1, 6),
+    'jogador3':randint(1, 6),
+    'jogador4':randint(1, 6)
+}
+ranking = []
 print('Sorteio de Números'.center(30))
-num['jogador1'] = randint(1, 6)
-num['jogador2'] = randint(1, 6)
-num['jogador3'] = randint(1, 6)
-num['jogador4'] = randint(1, 6)
 print('-='*30)
 for k, v in num.items():
     print(f'{k} tirou o número {v} no dado.')
+    sleep(.5)
 print('-='*30)
 print('Ranking'.upper().center(30))
-print('-='*30)
+ranking = sorted(num.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v}')
