b7ec32dd284206572bc917a49ee844bc56833a76
Ícaro Soares <icarosa21@gmail.com>
2025-11-25 08:50:08 -0300

    Criei o ex092.py

---
+++ ex092.py
@@ -0,0 +1,4 @@
+carteira_trabalho = {}
+carteira_trabalho['nome'] = str(input('Nome: ')).strip().title()
+carteira_trabalho['ano'] = int(input('Ano de nascimento: '))
+carteira_trabalho['número'] = int(input('Nº da CT [0 não existe]: '))
