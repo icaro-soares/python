20fb61f399962faa535e28f3a0990fd06417b0c7
Ícaro Soares <icarosa21@gmail.com>
2025-12-05 09:25:58 -0300

    Atualizei o ex092.py

--- ex092.py
+++ ex092.py
@@ -1,17 +1,17 @@
 from datetime import date
 
 
-ano_atual = date.today().year
-carteira_trabalho = {}
-carteira_trabalho['nome'] = str(input('Nome: ')).strip().title()
-carteira_trabalho['ano_nascimento'] = int(input('Ano de nascimento: '))
-carteira_trabalho['idade'] = ano_atual - carteira_trabalho['ano_nascimento']
-carteira_trabalho['salário'] = float(input('Qual seu salário? R$'))
-carteira_trabalho['número_ct'] = int(input('Nº da CT [0 não existe]: '))
-if carteira_trabalho['número_ct'] <= 0:
-    carteira_trabalho['número_ct'] = 'Não tem CT'
-carteira_trabalho['ano_contratação'] = int(input('Em que ano foi contratado(a)? '))
-carteira_trabalho['idade_contratação'] = carteira_trabalho['ano_contratação'] - carteira_trabalho['ano_nascimento']
+dados = {}
+dados['nome'] = str(input('Nome: '))
+dados['nasc'] = int(input('Nascimento: '))
+dados['idade'] = date.today().year - dados['nasc']
+dados['ct'] = int(input('Nº da CT (0 não existe): '))
+if dados['ct'] != 0:
+    dados['salario'] = float(input('Salário: R$'))
+    dados['contratação'] = int(input('Ano de contratação: '))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 65) - date.today().year)
+else:
+    dados['ct'] = 'Não existe'
 print('-='*30)
-for k, v in carteira_trabalho.items():
+for k, v in dados.items():
     print(f'{k}: {v}')
