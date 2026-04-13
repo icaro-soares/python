9638ed92e0febfcb537b70a9387268df5ecdb338
icaro-soares <151578696+icaro-soares@users.noreply.github.com>
2026-04-13 14:25:06 -0300

    Atualizei o repositório

--- ex038.py
+++ ex038.py
@@ -1,1 +1,9 @@
 
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+if n1 > n2:
+    print('O primeiro valor é maior')
+elif n1 < n2:
+    print('O segundo valor é maior')
+elif n1 == n2:
+    print('Os valores são iguais')
