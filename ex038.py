d7dd16201a02da2076248b1b402232ea2c58d2b4
icaro-soares <151578696+icaro-soares@users.noreply.github.com>
2026-04-13 14:18:22 -0300

    Atualizei o repositório

--- ex038.py
+++ ex038.py
@@ -1,14 +1,1 @@
-n1 = int(input('1º valor: '))
-n2 = int(input('2º valor: '))
-n3 = int(input('3º valor: '))
-maior = 0
-if n1 == n2 == n3:
-    print('Os valores são iguais!')
-else:
-    if n1 > n2 and n1 > n3:
-        maior = n1
-    elif n2 > n1 and n2 > n3:
-        maior = n2
-    elif n3 > n2 and n3 > n1:
-        maior = n3
-    print(f'O maior valor é {maior}')
+
