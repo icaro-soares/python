4e3a5b4e697f32ef1dbbdb08a8017f220a1c0e7b
icaro-soares <151578696+icaro-soares@users.noreply.github.com>
2026-04-16 08:54:02 -0300

    Atualizei o repositório

--- ex045.py
+++ ex045.py
@@ -1,55 +1,34 @@
 from random import randrange
 
 
-# Jogador 1 faz jogada
-j1 = int(input('''Faça sua escolha
-    [0 - Pedra]
-    [1 - Papel]
-    [2 - Tesoura]: '''))
-print('-='*30)
-if j1 == 0:
-    print('Você jogou pedra')
-elif j1 == 1:
-    print('Você jogou papel')
-elif j1 == 2:
-    print('Você jogou tesoura')
-else:
-    print('Jogada inválida, aplicação encerrada!')
-    exit()
-
-# Computador faz a jogada
+itens = ('Pedra', 'Papel', 'Tesoura')
+j1 = int(input('''Faça sua jogada:
+               [0 - Pedra]
+               [1 - Papel]
+               [2 - Tesoura]: '''))
 comp = randrange(0, 3)
-if comp == 0:
-    print('O computador jogou pedra')
-elif comp == 1:
-    print('O computador jogou papel')
-elif comp == 2:
-    print('O computador jogou tesoura')
-print('-='*30)
-
-# Condições
-
-# Pedra
-if j1 == 0: 
-    if comp == 0:
-        print('Empatou!')
-    elif comp == 1:
-        print('O cpu venceu!')
-    elif comp == 2:
-        print('O jogador venceu!')
-# Papel
-elif j1 == 1:
-    if comp == 0:
-        print('O jogador venceu!')
-    elif comp == 1:
-        print('Empatou!')
-    elif comp == 2:
-        print('O cpu venceu!')
-# Tesoura
-elif j1 == 2:
-    if comp == 0:
-        print('O cpu venceu!')
-    elif comp == 1:
-        print('O jogador venceu!')
-    elif comp == 2:
-        print('Empate!')
+print('-=' * 30)
+print(f'J1 jogou {itens[j1]}')
+print(f'CPU jogou {itens[comp]}')
+print('-=' * 30)
+if itens[j1] == 'Pedra':
+    if itens[comp] == 'Pedra':
+        print('Deu empate!')
+    elif itens[comp] == 'Papel':
+        print('Você perdeu')
+    elif itens[comp] == 'Tesoura':
+        print('Você ganhou!')
+elif itens[j1] == 'Papel':
+    if itens[comp] == 'Pedra':
+        print('Você ganhou!')
+    elif itens[comp] == 'Papel':
+        print('Deu empate!')
+    elif itens[comp] == 'Tesoura':
+        print('Você perdeu!')
+elif itens[j1] == 'Tesoura':
+    if itens[comp] == 'Pedra':
+        print('Você perdeu!')
+    elif itens[comp] == 'Papel':
+        print('Você ganhou!')
+    elif itens[comp] == 'Tesoura':
+        print('Deu empate!')
