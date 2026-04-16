b5c27e89aa7a2c7f5c40102552ef0bffae571df8
icaro-soares <151578696+icaro-soares@users.noreply.github.com>
2026-04-16 14:07:40 -0300

    Atualizei o ex049

--- ex049.py
+++ ex049.py
@@ -1,3 +1,7 @@
 n = int(input('Digite um número: '))
+print('-' * 30)
+print(f'Tabuada de {n}'.center(30))
+print('-' * 30)
 for c in range(1, 11):
-    print(f'{c:2} x {n:3} = {n*c:3}')
+    print(f'{c:2} x {n:3} = {n*c:3}'.center(30))
+print('-' * 30)
